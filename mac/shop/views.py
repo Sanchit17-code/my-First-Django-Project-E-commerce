bae5cb280ac94d2f20c564555d98bee49d51eeac
Sanchit Agrawal <sanchitagrawal429@gmail.com>
2022-07-31 11:47:30 +0530

    till showing multiple product slides on homepage

---
+++ mac/shop/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
+# Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slides':nSlides, 'range':range(nSlides), 'product':products}
+    return render(request, 'shop/index.html',params)
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    return HttpResponse("we are at contact")
+
+def tracker(request):
+    return HttpResponse("we are at tracker")
+
+def search(request):
+    return HttpResponse("we are at search")
+
+def productView(request):
+    return HttpResponse("we are at productView")
+
+def checkout(request):
+    return HttpResponse("we are at checkout")  
