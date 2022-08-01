4a93bacae6237e94d0cbf5c04476a45055b9048b
Sanchit Agrawal <sanchitagrawal429@gmail.com>
2022-08-01 08:08:48 +0530

    the actual category wire rendering of products

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -6,10 +6,17 @@ from math import ceil
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    allProds=[[products, range(1, len(products)), nSlides],[products, range(1, len(products)), nSlides]]
+    # products = Product.objects.all()
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+    allProds=[]
+    catProds = Product.objects.values('category','id')
+    cats = {item['category'] for item in catProds}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4)-(n//4))
+        allProds.append([prod,range(1,nSlides),nSlides])
     params={'allProds':allProds }
     return render(request, 'shop/index.html',params)
 
