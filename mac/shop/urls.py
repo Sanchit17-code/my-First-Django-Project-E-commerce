336854a99b8a06506d8d7f9ab8e5eb303e9a085d
Sanchit Agrawal <sanchitagrawal429@gmail.com>
2022-08-07 14:02:32 +0530

    contact page and its model

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="ShopHome"),
     path('about/', views.about, name="AboutUs"),
-    path('contact', views.contact, name="ContactUs"),
+    path('contact/', views.contact, name="ContactUs"),
     path('tracker/', views.tracker, name="TrackingStatus"),
     path('search/', views.search, name="Search"),
     path('products/<int:myid>', views.productView, name="ProductView"),
